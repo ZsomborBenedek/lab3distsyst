c7f6138664c7c18eb8055ad1e456cfc80cd85638
Benedek Zsombor Ákos <zsomborbeni@gmail.com>
2020-04-19 20:59:57 +0200

    Initial commit

---
+++ src/test/java/be/uantwerpen/lab3DistSystZsombor/Lab3DistSystZsomborApplicationTests.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.lab3DistSystZsombor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3DistSystZsomborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
